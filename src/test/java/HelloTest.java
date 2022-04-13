0608f45821355bb9753a0c6d27b4e3e728178cef
Zeynep Sena <zeynepssaltik@gmail.com>
2022-04-13 21:21:41 +0300

    added hello feature

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -3,4 +3,5 @@ public class HelloTest
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing Hello Feature");
+        System.out.println("Initial Commit more");
     }}
